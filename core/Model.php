2b9ced308ce8a04a16f247eaeddcc0bb7562ba5b
alvinlal <alvinzzz2001@gmail.com>
2021-02-11 12:35:19 +0530

    🎨 seperate functions for sql methods

--- core/Model.php
+++ core/Model.php
@@ -4,10 +4,20 @@ namespace app\core;
 use app\core\Database;
 
 class Model extends Database {
-	public function query(string $sql, array $args = []) {
+	public function select(string $sql, array $args = []) {
 		$stmt = parent::query($sql);
 		$stmt = parent::prepare($sql);
 		$stmt->execute($args);
 		return $stmt->fetchAll();
+	}
+	public function insert(string $sql, array $args = []) {
+		$stmt = parent::query($sql);
+		$stmt = parent::prepare($sql);
+		return $stmt->execute($args);
+	}
+	public function delete(string $sql, array $args = []) {
+		$stmt = parent::query($sql);
+		$stmt = parent::prepare($sql);
+		return $stmt->execute($args);
 	}
 }

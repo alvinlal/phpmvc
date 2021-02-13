3964192510dce548cbbf3f50febb897dfea85041
alvinlal <alvinzzz2001@gmail.com>
2021-02-13 17:49:58 +0530

    removed unwanted query method invocation

--- core/Model.php
+++ core/Model.php
@@ -5,18 +5,15 @@ use app\core\Database;
 
 class Model extends Database {
 	public function select(string $sql, array $args = []) {
-		$stmt = parent::query($sql);
 		$stmt = parent::prepare($sql);
 		$stmt->execute($args);
 		return $stmt->fetchAll();
 	}
 	public function insert(string $sql, array $args = []) {
-		$stmt = parent::query($sql);
 		$stmt = parent::prepare($sql);
 		return $stmt->execute($args);
 	}
 	public function delete(string $sql, array $args = []) {
-		$stmt = parent::query($sql);
 		$stmt = parent::prepare($sql);
 		return $stmt->execute($args);
 	}

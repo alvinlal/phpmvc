da3210bc92a0af126f1e630f19f841b882f7713f
alvinlal <alvinzzz2001@gmail.com>
2021-02-24 00:09:03 +0530

    added css file

--- views/pizza/add.php
+++ views/pizza/add.php
@@ -2,7 +2,7 @@
     <a href="add.php">
         <h4 class="center">Add a Pizza</h4>
     </a>
-    <form class="white" action="add" method="POST">
+    <form class="white" action="add" method="POST" enctype="multipart/form-data">
         <label>Pizza Title</label>
         <input type="text" name="title" value="<?=htmlspecialchars($data['title'] ?? '')?>">
         <div class="red-text">
@@ -13,7 +13,11 @@
         <div class="red-text">
             <?=$errors['ingredients'] ?? ''?>
         </div><br>
-        <div class="center">
+        <label style="margin:15px 0px">Photo (optional)</br>
+            <div style="margin:15px 0px"><input type="file" name="photo" /></div>
+        </label>
+        <input type="hidden" name="_csrf" value="<?=$_csrfToken?>" />
+        <div style="margin-top:50px" class="center">
             <input type="submit" value="submit" class="btn brand z-depth-0">
         </div>
     </form>
